0215f130301143955204cf6e73d05e5ec49a77a4
Bernhard Rosenkränzer <bero@lindev.ch>
2018-02-20 02:17:53 +0100

    Fix over-escaping in /etc/default/grub.OpenMandriva

--- distro-theme.spec
+++ distro-theme.spec
@@ -11,7 +11,7 @@
 
 Name:		distro-theme
 Version:	1.4.40
-Release:	4
+Release:	5
 Url:		https://abf.io/software/distro-theme
 Source0:	%{name}-%{version}.tar.xz
 Source1:	%{name}.rpmlintrc
--- generate-grub-config.sh
+++ generate-grub-config.sh
@@ -9,7 +9,7 @@ install -d %{buildroot}%{_sysconfdir}/default/
 cat > %{buildroot}%{_sysconfdir}/default/grub.${VENDOR} << EOF
 GRUB_THEME=/boot/grub2/themes/${VENDOR}/theme.txt
 GRUB_BACKGROUND=/boot/grub2/themes/${VENDOR}/terminal_background.png
-GRUB_DISTRIBUTOR=\"${DISTRIBUTION}\"
+GRUB_DISTRIBUTOR="${DISTRIBUTION}"
 %endif
 EOF
 EOS
